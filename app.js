cb2267aae5ac6518bfaa753c4973273823c8153c
algrow-learning <113255849+algrow-learning@users.noreply.github.com>
2022-09-11 22:54:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -292,7 +292,7 @@ app.post("/completedtodo", async (req, res) => {
 
 // End of Protected Routes
 
-app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, 'build')))
 // Set server to Listen State
 app.listen(port, () => {
   console.log("Hey app started at " + port);
